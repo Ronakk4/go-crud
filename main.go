6007268872e5c811f5cc3c5141452561efd3a227
Ronakk4 <102221785+Ronakk4@users.noreply.github.com>
2024-02-14 00:51:31 +0530

    final

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
-import(
+
+import (
+	
+	"encoding/json"
 	"fmt"
 	"log"
-	"encoding/json"
 	"net/http"
 	"strconv"
+	"math/rand"
 	"github.com/gorilla/mux"
 )
 
 type movie struct{
 	ID string `json:"id"`
 	Isbn string `json:"isbn"`
-	title string `json:"title`
+	Title string `json:"title"`
 	Director *Director `json:"director"`
 
 }
@@ -22,12 +25,76 @@ Lastname string `json:"Lastname"`
 
 
 }
-var Movie[] movie
+var movies[] movie
+
+func getMovies(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovies(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params:=mux.Vars(r)
+	for index,item := range movies{
+		if item.ID==params["id"]{
+			movies=append(movies[:index],movies[index+1:]...)
+			 break
+		}
+		
+	}
+	json.NewEncoder(w).Encode(movies)
+
+}
+func getMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params :=mux.Vars(r)
+
+	for _,item:=range movies{
+		if item.ID==params["id"]{
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+
+}
+func createMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	var Movie movie
+	_=json.NewDecoder(r.Body).Decode(&Movie)
+	Movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies=append(movies, Movie)
+	json.NewEncoder(w).Encode(Movie)
+
+
+
+
+}
+
+func updateMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params :=mux.Vars(r)
+
+	for index,item:=range movies{
+		if item.ID==params["id"]{
+			movies=append(movies[:index],movies[index+1:]...)
+			var Movie movie
+			_=json.NewDecoder(r.Body).Decode(&Movie)
+			Movie.ID=params["id"]
+			movies =append(movies, Movie)
+			json.NewEncoder(w).Encode(Movie)
+
+		}
+	}
+
+}
+
 
 func main(){
 	r :=mux.NewRouter()
-	r.HandleFunc("/movies,",getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}",getMoviesByID).Methods("GET")
+	movies=append(movies,movie{ID:"1",Isbn:"4322",Title:"Movie one",Director:&Director{Firstname:"John",Lastname:"Doe"}})
+	movies=append(movies,movie{ID:"2",Isbn:"43222",Title:"Movie two",Director:&Director{Firstname:"Raj",Lastname:"Mehta"}})
+	
+	r.HandleFunc("/movies",getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
 	r.HandleFunc("/movies",createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}",updateMovie).Methods("DELETE")
